1e355fd88f3ffebac10206828c8e6593d8c06416
resoul <resoul@bk.ru>
2018-06-22 17:10:46 +0300

    override Html, ActiveForm

---
+++ system/ua/helpers/Html.php
@@ -0,0 +1,14 @@
+<?php
+namespace ua\helpers;
+
+use yii\helpers\BaseHtml;
+
+/**
+ * Html class basen on BaseHtml
+ *
+ * @author ReSoul <roberts.mark1985@gmail.com>
+ * @ver 0.01
+ * @since 0.1
+ */
+class Html extends BaseHtml
+{}
